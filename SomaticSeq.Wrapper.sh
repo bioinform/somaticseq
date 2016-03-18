746557b19a5867898b8433eeae7c474735698d7c
Li Tai Fang <ltfang@bina.com>
2016-03-17 23:24:58 -0700

    beautify

--- SomaticSeq.Wrapper.sh
+++ SomaticSeq.Wrapper.sh
@@ -10,50 +10,28 @@ MYDIR="$( cd "$( dirname "$0" )" && pwd )"
 while getopts "o:M:I:V:v:J:S:D:U:g:c:d:s:G:T:N:C:x:R:i:z:Z:" opt
 do
     case $opt in
-        o)
-            merged_dir=$OPTARG;;
-	M)
-	    mutect_vcf=$OPTARG;;
-	I)
-	    indelocator_vcf=$OPTARG;;
-	V)
-	    varscan_vcf=$OPTARG;;
-	v)
-	    varscan_indel_vcf=$OPTARG;;
-	J)
-	    jsm_vcf=$OPTARG;;
-	S)
-	    sniper_vcf=$OPTARG;;
-	D)
-	    vardict_vcf=$OPTARG;;
-	U)
-	    muse_vcf=$OPTARG;;
-	g)
-	    hg_ref=$OPTARG;;
-	c)
-	    cosmic=$OPTARG;;
-	d)
-	    dbsnp=$OPTARG;;
-	s)
-	    snpeff_dir=$OPTARG;;
-	G)
-	    gatk=$OPTARG;;
-	T)
-	    tbam=$OPTARG;;
-	N)
-	    nbam=$OPTARG;;
-	C)
-	    snpclassifier=$OPTARG;;
-	x)
-	    indelclassifier=$OPTARG;;
-	R)
-	    ada_r_script=$OPTARG;;
-	i)
-	    masked_region=$OPTARG;;
-	z)
-	    indelgroundtruth=$OPTARG;;
-	Z)
-	    snpgroundtruth=$OPTARG;;
+        o) merged_dir=$OPTARG;;
+	M) mutect_vcf=$OPTARG;;
+	I) indelocator_vcf=$OPTARG;;
+	V) varscan_vcf=$OPTARG;;
+	v) varscan_indel_vcf=$OPTARG;;
+	J) jsm_vcf=$OPTARG;;
+	S) sniper_vcf=$OPTARG;;
+	D) vardict_vcf=$OPTARG;;
+	U) muse_vcf=$OPTARG;;
+	g) hg_ref=$OPTARG;;
+	c) cosmic=$OPTARG;;
+	d) dbsnp=$OPTARG;;
+	s) snpeff_dir=$OPTARG;;
+	G) gatk=$OPTARG;;
+	T) tbam=$OPTARG;;
+	N) nbam=$OPTARG;;
+	C) snpclassifier=$OPTARG;;
+	x) indelclassifier=$OPTARG;;
+	R) ada_r_script=$OPTARG;;
+	i) masked_region=$OPTARG;;
+	z) indelgroundtruth=$OPTARG;;
+	Z) snpgroundtruth=$OPTARG;;
     esac
 done
 
