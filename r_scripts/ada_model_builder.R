2f64a9346aaae8e00c6c90a68d8981252ffd9f6e
Li Tai Fang <li_tai.fang@bina.roche.com>
2016-03-18 15:14:47 -0700

    format clean up

--- r_scripts/ada_model_builder.R
+++ r_scripts/ada_model_builder.R
@@ -4,35 +4,21 @@ require("ada")
 
 args <- commandArgs(TRUE)
 
-training_data_filename = args[1]
+train_filename = args[1]
 
 ##### Main (entry point)
-
-train_filename = paste(training_data_filename)
-
 train_data = read.table(train_filename, header=TRUE)
-
 
 if (!(1 %in% train_data$TrueVariant_or_False && 0 %in% train_data$TrueVariant_or_False)) {
 stop("In training mode, there must be both true positives and false positives in the call set.")
 }
 
-if (FALSE) {
-print("Updating missing values...")
-train_data <- set_missing_values(train_data)
-
-}else {
-
 train_data <- train_data[,-c(1, 2, 3, 4, 5)]
-
-}
-
 
 train_data[,'REF'] <- NULL
 train_data[,'ALT'] <- NULL
 train_data[,'if_COSMIC'] <- NULL
 train_data[,'COSMIC_CNT'] <- NULL
-
 
 model_formula <- as.formula(TrueVariant_or_False ~ .)
 
@@ -40,7 +26,6 @@ print("Fitting model...")
 ada.model <- ada(model_formula, data = train_data, iter = 500)
 
 save(ada.model, file = paste(training_data_filename, ".Classifier.RData", sep="") )
-
 
 print(ada.model)
 
