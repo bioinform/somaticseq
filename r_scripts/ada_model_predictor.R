bd84b525594f8212a5aaf4d7e1caa1d788f5c192
Li Tai Fang <li_tai.fang@bina.roche.com>
2016-07-21 23:30:43 -0700

    formatting

--- r_scripts/ada_model_predictor.R
+++ r_scripts/ada_model_predictor.R
@@ -12,20 +12,17 @@ test_data_ = read.table(test_filename, header=TRUE)
 
 test_data <- test_data_
 
+# Remove from training identities, i.e., CHROM, POS, etc.
 test_data <- test_data[,-c(1, 2, 3, 4, 5)]
-test_data[,'TrueVariant_or_False'] <- NULL
 
+test_data[,'TrueVariant_or_False'] <- NULL
 test_data[,'REF'] <- NULL
 test_data[,'ALT'] <- NULL
 
+# A bug where sometimes SOR gets classified as "types"
 test_data$SOR <- as.numeric(test_data$SOR)
 
-#test_data[,'if_dbsnp'] <- NULL
-#test_data[,'BAF'] <- NULL
-#test_data[,'COMMON'] <- NULL
-#test_data[,'G5'] <- NULL
-#test_data[,'G5A'] <- NULL
-
+# Handle empty input data
 if ( nrow(test_data)>=1 ) {
     load( trained_model )
     ada.pred <- predict(ada.model, newdata = test_data, type="both")
