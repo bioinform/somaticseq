379945896847dad5ac9754b2044b1056201c1687
Li Tai Fang <li_tai.fang@bina.roche.com>
2017-08-21 22:18:22 -0700

    bamsurgeon

--- utilities/dockered_pipelines/bamSurgeon/singleThread/bamsurgeon_addindels.sh
+++ utilities/dockered_pipelines/bamSurgeon/singleThread/bamsurgeon_addindels.sh
@@ -163,6 +163,13 @@ echo "bedtools sort -header -faidx /mnt/${HUMAN_REFERENCE}.fai \\" >> $out_scrip
 echo "> ${outdir}/synthetic_indels.vcf" >> $out_script
 echo "" >> $out_script
 
+echo "docker run --rm -v /:/mnt -i lethalfang/somaticseq:2.3.1 \\" >> $out_script
+echo "java -jar /opt/GATK/GenomeAnalysisTK.jar -T LeftAlignAndTrimVariants \\" >> $out_script
+echo "-R /mnt/${HUMAN_REFERENCE} \\" >> $out_script
+echo "--variant /mnt/${outdir}/synthetic_indels.vcf \\" >> $out_script
+echo "| egrep -v '^[0-9]+ variants|^INFO' > ${outdir}/synthetic_indels.leftaligned.vcf"
+echo "" >> $out_script
+
 echo "docker run -v /:/mnt -u $UID --rm -i lethalfang/samtools:1.3.1 \\" >> $out_script
 echo "samtools sort -m 4G --reference /mnt/${HUMAN_REFERENCE} -o /mnt/${outdir}/${outbam} /mnt/${outdir}/unsorted.${outbam}" >> $out_script
 
