3c789bc772263ddfe912741b3ac506f41c6215e2
Li Tai Fang <li_tai.fang@bina.roche.com>
2017-08-25 18:25:40 -0700

    docker

---
+++ utilities/dockered_pipelines/singleThread/submit_VarScan2.sh
@@ -0,0 +1,122 @@
+set -e
+
+OPTS=`getopt -o o: --long out-dir:,out-vcf:,tumor-bam:,normal-bam:,human-reference:,selector:,action:,VAF -n 'submit_VarScan2.sh'  -- "$@"`
+
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+
+echo "$OPTS"
+eval set -- "$OPTS"
+
+MYDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+timestamp=$( date +"%Y-%m-%d_%H-%M-%S_%N" )
+VAF=0.20
+action=echo
+
+while true; do
+    case "$1" in
+   --out-vcf )
+        case "$2" in
+            "") shift 2 ;;
+            *)  outvcf=$2 ; shift 2 ;;
+        esac ;;
+
+   --out-dir )
+        case "$2" in
+            "") shift 2 ;;
+            *)  outdir=$2 ; shift 2 ;;
+        esac ;;
+
+    --tumor-bam )
+        case "$2" in
+            "") shift 2 ;;
+            *)  tumor_bam=$2 ; shift 2 ;;
+        esac ;;
+
+    --normal-bam )
+        case "$2" in
+            "") shift 2 ;;
+            *)  normal_bam=$2 ; shift 2 ;;
+        esac ;;
+
+    --human-reference )
+        case "$2" in
+            "") shift 2 ;;
+            *)  HUMAN_REFERENCE=$2 ; shift 2 ;;
+        esac ;;
+
+    --selector )
+        case "$2" in
+            "") shift 2 ;;
+            *) SELECTOR=$2 ; shift 2 ;;
+        esac ;;
+
+    --VAF )
+        case "$2" in
+            "") shift 2 ;;
+            *) VAF=$2 ; shift 2 ;;
+        esac ;;
+
+    --action )
+        case "$2" in
+            "") shift 2 ;;
+            *) action=$2 ; shift 2 ;;
+        esac ;;
+
+    -- ) shift; break ;;
+    * ) break ;;
+    esac
+
+done
+
+
+logdir=${outdir}/logs
+mkdir -p ${logdir}
+
+varscan2_script=${outdir}/logs/varscan2_${timestamp}.cmd
+
+selector_text=''
+if [[ -r $SELECTOR ]]; then
+    selector_text="-l /mnt/${SELECTOR}"
+fi
+
+
+echo "#!/bin/bash" > $varscan2_script
+echo "" >> $varscan2_script
+
+echo "#$ -o ${logdir}" >> $varscan2_script
+echo "#$ -e ${logdir}" >> $varscan2_script
+echo "#$ -S /bin/bash" >> $varscan2_script
+echo '#$ -l h_vmem=9G' >> $varscan2_script
+echo 'set -e' >> $varscan2_script
+echo "" >> $varscan2_script
+
+echo 'echo -e "Start at `date +"%Y/%m/%d %H:%M:%S"`" 1>&2' >> $varscan2_script
+echo "" >> $varscan2_script
+
+echo "docker run --rm -u $UID -v /:/mnt -i lethalfang/samtools:0.1.19 \\" >> $varscan2_script
+echo "samtools mpileup \\" >> $varscan2_script
+echo "-B -q 25 -Q 20 -f $selector_text \\" >> $varscan2_script
+echo "/mnt/${HUMAN_REFERENCE} \\" >> $varscan2_script
+echo "/mnt/${normal_bam} \\" >> $varscan2_script
+echo "> ${outdir}/normal.pileup" >> $varscan2_script
+
+echo "" >> $varscan2_script
+
+echo "docker run --rm -u $UID -v /:/mnt -i lethalfang/samtools:0.1.19 \\" >> $varscan2_script
+echo "samtools mpileup \\" >> $varscan2_script
+echo "-B -q 25 -Q 20 -f $selector_text \\" >> $varscan2_script
+echo "/mnt/${HUMAN_REFERENCE} \\" >> $varscan2_script
+echo "/mnt/${tumor_bam} \\" >> $varscan2_script
+echo "> ${outdir}/normal.pileup" >> $varscan2_script
+
+echo "" >> $varscan2_script
+
+
+echo "docker run --rm -u $UID -v /:/mnt -i djordjeklisic/sbg-varscan2:v1 \\" >> $varscan2_script
+echo "java -Xmx8g -jar VarScan2.3.7.jar \\" >> $varscan2_script
+echo "/mnt/${outdir}/VarScan2 --output-vcf 1" >> $varscan2_script
+
+
+
+${action} $varscan2_script
