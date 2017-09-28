004522dfb9057d2466431e2317a56ce0b61b9a65
Li Tai Fang <li_tai.fang@bina.roche.com>
2017-09-28 01:39:36 -0700

    more mem

--- utilities/dockered_pipelines/alignments/bwa_mem_pe.sh
+++ utilities/dockered_pipelines/alignments/bwa_mem_pe.sh
@@ -114,7 +114,7 @@ then
     echo "#$ -o ${logdir}" >> $out_script
     echo "#$ -e ${logdir}" >> $out_script
     echo "#$ -S /bin/bash" >> $out_script
-    echo '#$ -l h_vmem=12G' >> $out_script
+    echo '#$ -l h_vmem=16G' >> $out_script
     echo "#$ -pe smp ${threads}" >> $out_script
     echo 'set -e' >> $out_script
 fi
