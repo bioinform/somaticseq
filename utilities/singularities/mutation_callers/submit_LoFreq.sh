3d9f47d813873287a8a673d8f6e20eeaad3f7929
Li Tai Fang <li_tai.fang@bina.roche.com>
2017-11-03 23:06:04 -0700

    singularity

---
+++ utilities/singularities/mutation_callers/submit_LoFreq.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+# Use getopt instead of getopts for long options
+
+set -e
+
+OPTS=`getopt -o o: --long out-dir:,out-vcf:,tumor-bam:,normal-bam:,human-reference:,selector:,dbsnp:,extra-arguments:,MEM:,action: -n 'submit_LoFreq.sh'  -- "$@"`
+
+if [ $? != 0 ] ; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+
+#echo "$OPTS"
+eval set -- "$OPTS"
+
+MYDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+timestamp=$( date +"%Y-%m-%d_%H-%M-%S_%N" )
+VAF=0.05
+action=echo
+MEM=12
+
+while true; do
+    case "$1" in
+    --out-vcf )
+        case "$2" in
+            "") shift 2 ;;
+            *)  outvcf=$2 ; shift 2 ;;
+        esac ;;
+
+    --out-dir )
+        case "$2" in
+            "") shift 2 ;;
+            *)  outdir=$2 ; shift 2 ;;
+        esac ;;
+
+    --tumor-bam )
+        case "$2" in
+            "") shift 2 ;;
+            *)  tumor_bam=$2 ; shift 2 ;;
+        esac ;;
+
+    --normal-bam )
+        case "$2" in
+            "") shift 2 ;;
+            *)  normal_bam=$2 ; shift 2 ;;
+        esac ;;
+
+    --human-reference )
+        case "$2" in
+            "") shift 2 ;;
+            *)  HUMAN_REFERENCE=$2 ; shift 2 ;;
+        esac ;;
+
+    --selector )
+        case "$2" in
+            "") shift 2 ;;
+            *) SELECTOR=$2 ; shift 2 ;;
+        esac ;;
+
+    --extra-arguments )
+        case "$2" in
+            "") shift 2 ;;
+            *) extra_arguments=$2 ; shift 2 ;;
+        esac ;;
+
+    --MEM )
+        case "$2" in
+            "") shift 2 ;;
+            *) MEM=$2 ; shift 2 ;;
+        esac ;;
+
+    --dbsnp )
+        case "$2" in
+            "") shift 2 ;;
+            *) dbsnp=$2 ; shift 2 ;;
+        esac ;;
+
+    --action )
+        case "$2" in
+            "") shift 2 ;;
+            *) action=$2 ; shift 2 ;;
+        esac ;;
+
+    -- ) shift; break ;;
+    * ) break ;;
+    esac
+
+done
+
+vcf_prefix=${outvcf%\vcf}
+
+logdir=${outdir}/logs
+mkdir -p ${logdir}
+
+out_script=${outdir}/logs/lofreq_${timestamp}.cmd
+
+echo "#!/bin/bash" > $out_script
+echo "" >> $out_script
+
+echo "#$ -o ${logdir}" >> $out_script
+echo "#$ -e ${logdir}" >> $out_script
+echo "#$ -S /bin/bash" >> $out_script
+echo "#$ -l h_vmem=${MEM}G" >> $out_script
+echo 'set -e' >> $out_script
+echo "" >> $out_script
+
+echo 'echo -e "Start at `date +"%Y/%m/%d %H:%M:%S"`" 1>&2' >> $out_script
+echo "" >> $out_script
+
+echo "singularity exec --bind /:/mnt docker://lethalfang/lofreq:2.1.3.1 \\" >> $out_script
+echo "lofreq somatic \\" >> $out_script
+echo "-t /mnt/${tumor_bam} \\" >> $out_script
+echo "-n /mnt/${normal_bam} \\" >> $out_script
+echo "--call-indels \\" >> $out_script
+echo "-l /mnt/${SELECTOR} \\" >> $out_script
+echo "-f /mnt/${HUMAN_REFERENCE} \\" >> $out_script
+echo "-o /mnt/${outdir}/${vcf_prefix} \\" >> $out_script
+echo "${extra_arguments} \\" >> $out_script
+echo "-d /mnt/${dbsnp}.gz" >> $out_script
+
+echo "" >> $out_script
+echo 'echo -e "Done at `date +"%Y/%m/%d %H:%M:%S"`" 1>&2' >> $out_script
+
+${action} $out_script
