2b9c34f4afa36f4bbef6f8c55f9d0106ba437bca
Li Tai Fang <li_tai.fang@bina.roche.com>
2018-09-12 17:46:21 -0700

    3.0.1

--- vcfModifier/vcfIntersector.py
+++ vcfModifier/vcfIntersector.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import sys, os, argparse, gzip, re, subprocess
+import sys, os, argparse, gzip, re, subprocess, uuid
 
 MY_DIR = os.path.dirname(os.path.realpath(__file__))
 PRE_DIR = os.path.join(MY_DIR, os.pardir)
@@ -37,6 +37,52 @@ def bed_exclude(infile, exclusion_region, outfile):
         outfile = None
         
     return outfile
+
+
+
+
+def bed_intersector(infile, outfile, inclusion_region=None, exclusion_region=None):
+    
+    assert infile != outfile
+    from shutil import copyfile
+    
+    # Get the input file name minus the extention, and also get the extension
+    infile_noext = re.sub(r'\.\w+$', '', infile)
+    file_ext     = re.search(r'\.\w+$',  infile).group()
+    
+    temp_files   = []
+    
+    if inclusion_region:
+        
+        included_temp_file = infile_noext + uuid.uuid4().hex + file_ext
+        
+        exit_code = os.system( 'intersectBed -header -a {} -b {} | uniq > {}'.format(infile, inclusion_region, included_temp_file) )
+        assert exit_code == 0
+        
+        infile  = included_temp_file
+        temp_files.append( included_temp_file )
+    
+    
+    if exclusion_region:
+        
+        excluded_temp_file = infile_noext + uuid.uuid4().hex + file_ext
+        
+        exit_code = os.system( 'intersectBed -header -a {} -b {} -v | uniq > {}'.format(infile, exclusion_region, outfile) )
+        assert exit_code == 0
+    
+    
+    if inclusion_region and not exclusion_region:
+        copyfile(included_temp_file, outfile)
+    
+    elif not (inclusion_region or exclusion_region):
+        copyfile(infile, outfile)
+    
+    
+    for file_i in temp_files:
+        os.remove( file_i )
+        
+    return outfile
+
 
 
 # Use utilities/vcfsorter.pl fa.dict unsorted.vcf > sorted.vcf
