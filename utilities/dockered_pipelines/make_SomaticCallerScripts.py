be44b0acd9cf00987da77e4e3df6f601726faf1b
Li Tai Fang <li_tai.fang@roche.com>
2019-02-21 01:31:59 -0800

    wip

--- utilities/dockered_pipelines/make_SomaticCallerScripts.py
+++ utilities/dockered_pipelines/make_SomaticCallerScripts.py
@@ -69,7 +69,7 @@ def run():
     parser.add_argument('--jsm-classify-arguments',       type=str, help='extra parameters for JointSNVMix2 classify',     default='')
     parser.add_argument('--somaticsniper-arguments',      type=str, help='extra parameters for SomaticSniper',             default='')
     parser.add_argument('--vardict-arguments',            type=str, help='extra parameters for VarDict',                   default='')
-    parser.add_argument('--muse-arguments',               type=str, help='extra parameters',                               default='-G')
+    parser.add_argument('--muse-arguments',               type=str, help='extra parameters',                               default='')
     parser.add_argument('--lofreq-arguments',             type=str, help='extra parameters for LoFreq',                    default='')
     parser.add_argument('--scalpel-discovery-arguments',  type=str, help='extra parameters for Scalpel discovery',         default='')
     parser.add_argument('--scalpel-export-arguments',     type=str, help='extra parameters for Scalpel export',            default='')
@@ -91,23 +91,6 @@ def run():
 
 
 
-
-def splitRegions(nthreads, outfiles, bed=None, fai=None):
-
-    assert bed or fai
-    if fai and not bed:
-        bed = split_bed.fai2bed(fai, outfiles)
-
-    if nthreads > 1:
-        writtenBeds = split_bed.split(bed, outfiles, nthreads)
-    elif nthreads == 1:
-        writtenBeds = bed
-
-    return writtenBeds
-
-
-
-
 def run_MuTect2(input_parameters, mem=4, nt=4, outvcf='MuTect2.vcf'):
     
     logdir         = input_parameters['output_directory'] + os.sep + 'logs'
@@ -115,15 +98,13 @@ def run_MuTect2(input_parameters, mem=4, nt=4, outvcf='MuTect2.vcf'):
 
     with open(outfile, 'w') as out:
 
-        out.write( "#!/bin/bash\n" )
-        out.write( '\n' )
+        out.write( "#!/bin/bash\n\n" )
         
         out.write( '#$ -o {LOGDIR}\n'.format(LOGDIR=logdir) )
         out.write( '#$ -e {LOGDIR}\n'.format(LOGDIR=logdir) )
         out.write( '#$ -S /bin/bash\n' )
         out.write( '#$ -l h_vmem={}G\n'.format(mem) )
-        out.write( 'set -e\n' )
-        out.write( '\n' )
+        out.write( 'set -e\n\n' )
         
         out.write( 'echo -e "Start at `date +"%Y/%m/%d %H:%M:%S"`" 1>&2\n\n' )
 
@@ -160,7 +141,6 @@ def run_MuTect2(input_parameters, mem=4, nt=4, outvcf='MuTect2.vcf'):
 
 
 
-
 def run_VarScan2(input_parameters, mem=4, minVAF=0.10, minMQ=25, minBQ=20, outvcf='VarScan2.vcf'):
     
     logdir         = input_parameters['output_directory'] + os.sep + 'logs'
@@ -169,7 +149,6 @@ def run_VarScan2(input_parameters, mem=4, minVAF=0.10, minMQ=25, minBQ=20, outvc
 
     if input_parameters['minimum_VAF']:
         minVAF = input_parameters['minimum_VAF']
-
 
     selector_text  = '-l /mnt/{}'.format(input_parameters['inclusion_region']) if input_parameters['inclusion_region'] else ''
 
@@ -237,7 +216,6 @@ def run_VarScan2(input_parameters, mem=4, minVAF=0.10, minMQ=25, minBQ=20, outvc
 
 
 
-
 def run_JointSNVMix2(input_parameters, mem=8, skip_size=10000, converge_threshold=0.01, outvcf='JointSNVMix2.vcf'):
     
     logdir         = input_parameters['output_directory'] + os.sep + 'logs'
@@ -297,7 +275,7 @@ def run_JointSNVMix2(input_parameters, mem=8, skip_size=10000, converge_threshol
             out.write( '\n\ni=1\n' )
             out.write( 'while [[ $i -le {} ]]\n'.format(input_parameters['threads']) )
             out.write( 'do\n' )
-            out.write( '    docker run --rm -v /:/mnt -u $UID lethalfang/bedtools:2.26.0 bash -c "bedtools intersect -a /mnt/{OUTDIR}/{OUTVCF} -b /mnt/{OUTDIR}/${i}/${i}.bed -header | uniq > /mnt/{OUTDIR}/${i}/{OUTVCF}"\n'.format(OUTDIR=input_parameters['output_directory'], OUTVCF=outvcf, i='{i}') )
+            out.write( '    docker run --rm -v /:/mnt -u $UID lethalfang/bedtools:2.26.0 bash -c "bedtools intersect -a /mnt/{OUTDIR}/{OUTVCF} -b /mnt/{OUTDIR}/${{i}}/${{i}}.bed -header | uniq > /mnt/{OUTDIR}/${{i}}/{OUTVCF}"\n'.format(OUTDIR=input_parameters['output_directory'], OUTVCF=outvcf,) )
             out.write( '    i=$(( $i + 1 ))\n' )
             out.write( 'done\n' )
         
@@ -307,7 +285,6 @@ def run_JointSNVMix2(input_parameters, mem=8, skip_size=10000, converge_threshol
     returnCode = os.system('{} {}'.format(input_parameters['action'], outfile) )
 
     return returnCode
-
 
 
 
@@ -341,7 +318,7 @@ def run_SomaticSniper(input_parameters, MQ=1, somaticQuality=15, prior=0.00001, 
             out.write( '\n\ni=1\n' )
             out.write( 'while [[ $i -le {} ]]\n'.format(input_parameters['threads']) )
             out.write( 'do\n' )
-            out.write( '    docker run --rm -v /:/mnt -u $UID lethalfang/bedtools:2.26.0 bash -c "bedtools intersect -a /mnt/{OUTDIR}/{OUTVCF} -b /mnt/{OUTDIR}/${i}/${i}.bed -header | uniq > /mnt/{OUTDIR}/${i}/{OUTVCF}"\n'.format(OUTDIR=input_parameters['output_directory'], OUTVCF=outvcf, i='{i}') )
+            out.write( '    docker run --rm -v /:/mnt -u $UID lethalfang/bedtools:2.26.0 bash -c "bedtools intersect -a /mnt/{OUTDIR}/{OUTVCF} -b /mnt/{OUTDIR}/${{i}}/${{i}}.bed -header | uniq > /mnt/{OUTDIR}/${{i}}/{OUTVCF}"\n'.format(OUTDIR=input_parameters['output_directory'], OUTVCF=outvcf,) )
             out.write( '    i=$(( $i + 1 ))\n' )
             out.write( 'done\n' )
         
@@ -356,7 +333,6 @@ def run_SomaticSniper(input_parameters, MQ=1, somaticQuality=15, prior=0.00001, 
 
 
 
-
 def run_VarDict(input_parameters, mem=14, minVAF=0.05, outvcf='VarDict.vcf'):
     
     logdir         = input_parameters['output_directory'] + os.sep + 'logs'
@@ -364,15 +340,23 @@ def run_VarDict(input_parameters, mem=14, minVAF=0.05, outvcf='VarDict.vcf'):
     
     if input_parameters['minimum_VAF']:
         minVAF = input_parameters['minimum_VAF']
-    
 
     total_bases = 0
     num_lines   = 0
 
     if input_parameters['inclusion_region']:
         bed_file = input_parameters['inclusion_region']
-    else:
         
+        with open(bed_file) as bed:
+            line_i = bed.readline().rstrip()
+            while line_i.startswith('track'):
+                line_i = bed.readline().rstrip()
+            while line_i:
+                item = line_i.rstrip().split('\t')
+                total_bases = total_bases + int(item[2]) - int(item[1])
+                num_lines += 1
+                line_i = bed.readline().rstrip()
+    else:
         fai_file = input_parameters['genome_reference'] + '.fai'
         bed_file = '{}/{}'.format(input_parameters['output_directory'], 'genome.bed')
         
@@ -384,28 +368,127 @@ def run_VarDict(input_parameters, mem=14, minVAF=0.05, outvcf='VarDict.vcf'):
                 total_bases += int( item[1] )
                 num_lines   += 1
                 
-                wgs_bed.write( '{}\t{}\t{}\n'.format(item[0], '0', item[1])
-    
-    
-        
-    
+                wgs_bed.write( '{}\t{}\t{}\n'.format(item[0], '0', item[1]) )
     
     with open(outfile, 'w') as out:
 
-        out.write( "#!/bin/bash\n" )
-        out.write( '\n' )
+        out.write( "#!/bin/bash\n\n" )
         
         out.write( '#$ -o {LOGDIR}\n'.format(LOGDIR=logdir) )
         out.write( '#$ -e {LOGDIR}\n'.format(LOGDIR=logdir) )
         out.write( '#$ -S /bin/bash\n' )
         out.write( '#$ -l h_vmem={}G\n'.format(mem) )
-        out.write( 'set -e\n' )
-        out.write( '\n' )
+        out.write( 'set -e\n\n' )
         
         out.write( 'echo -e "Start at `date +"%Y/%m/%d %H:%M:%S"`" 1>&2\n\n' )
 
+        # Decide if Bed file needs to be "split" such that each line has a small enough region
+        if total_bases/num_lines > 50000:
+            out.write( 'docker run --rm -v /:/mnt -u $UID --memory {MEM}G lethalfang/somaticseq:{VERSION} \\\n'.format(MEM=mem, VERSION=VERSION) )
+            out.write( '/opt/somaticseq/utilities/split_mergedBed.py \\\n' )
+            out.write( '-infile /mnt/{SELECTOR} -outfile /mnt/{OUTDIR}/split_regions.bed\n\n'.format(SELECTOR=bed_file, OUTDIR=input_parameters['output_directory']) )
+
+            bed_file = '{OUTDIR}/split_regions.bed'.format( OUTDIR=input_parameters['output_directory'] )
+
+        out.write( 'docker run --rm -v /:/mnt -u $UID --memory {MEM}G lethalfang/vardictjava:1.5.2 bash -c \\\n'.format(MEM=mem) )
+        out.write( '"/opt/VarDict-1.5.2/bin/VarDict \\\n' )
+        out.write( '{EXTRA_ARG} \\\n'.format(EXTRA_ARG=input_parameters['vardict_arguments']) )
+        out.write( '-G /mnt/{HUMAN_REFERENCE} \\\n'.format(HUMAN_REFERENCE=input_parameters['genome_reference']) )
+        out.write( '-f {VAF} -h \\\n'.format(VAF=minVAF) )
+        out.write( '-b \'/mnt/{TBAM}|/mnt/{NBAM}\' \\\n'.format(TBAM=input_parameters['tumor_bam'], NBAM=input_parameters['normal_bam']) )
+        out.write( '-Q 1 -c 1 -S 2 -E 3 -g 4 /mnt/{INTPUT_BED} \\\n'.format(INTPUT_BED=bed_file) )
+        out.write( '> /mnt/{OUTDIR}/vardict.var"\n\n'.format(OUTDIR=input_parameters['output_directory']) )
+        
+        out.write( 'docker run --rm -v /:/mnt -u $UID --memory {MEM}G lethalfang/vardictjava:1.5.2 \\\n'.format(MEM=mem) )
+        out.write( 'bash -c "cat /mnt/{OUTDIR}/vardict.var | awk \'NR!=1\' | /opt/VarDict/testsomatic.R | /opt/VarDict/var2vcf_paired.pl -N \'TUMOR|NORMAL\' -f {VAF} \\\n'.format(OUTDIR=input_parameters['output_directory'], VAF=minVAF ) )
+        out.write( '> /mnt/{OUTDIR}/{OUTVCF}"\n\n'.format(OUTDIR=input_parameters['output_directory'], OUTVCF=outvcf) )
+
+        out.write( '\necho -e "Done at `date +"%Y/%m/%d %H:%M:%S"`" 1>&2\n' )
+        
+    returnCode = os.system('{} {}'.format(input_parameters['action'], outfile) )
+
+    return returnCode
 
 
+
+def run_MuSE(input_parameters, mem=8, outvcf='MuSE.vcf'):
+
+    logdir         = input_parameters['output_directory'] + os.sep + 'logs'
+    outfile        = logdir + os.sep + 'muse.{}.cmd'.format(ts)
+
+    statFlag = '-E' if input_parameters['exome_setting'] else '-G'
+    
+    with open(outfile, 'w') as out:
+
+        out.write( "#!/bin/bash\n\n" )
+        
+        out.write( '#$ -o {LOGDIR}\n'.format(LOGDIR=logdir) )
+        out.write( '#$ -e {LOGDIR}\n'.format(LOGDIR=logdir) )
+        out.write( '#$ -S /bin/bash\n' )
+        out.write( '#$ -l h_vmem={}G\n'.format(mem) )
+        out.write( 'set -e\n\n' )
+        
+        out.write( 'echo -e "Start at `date +"%Y/%m/%d %H:%M:%S"`" 1>&2\n\n' )
+
+        out.write( 'cat {SELECTOR} | awk -F "\\t" \'{{print $1 "\\t" $2 "\\t" $3}}\' > {OUTDIR}/bed_3columns.bed\n\n'.format(SELECTOR=input_parameters['inclusion_region'], OUTDIR=input_parameters['output_directory'] ) )
+        
+        out.write( 'docker run --rm -v /:/mnt -u $UID --memory {}G marghoob/muse:1.0rc_c \\\n'.format(mem) )
+        out.write( 'MuSEv1.0rc_submission_c039ffa call \\\n' )
+        out.write( '-O /mnt/{OUTDIR}/MuSE \\\n'.format(OUTDIR=input_parameters['output_directory']) )
+        out.write( '-l /mnt/{OUTDIR}/bed_3columns.bed \\\n'.format(OUTDIR=input_parameters['output_directory']) )
+        out.write( '-f /mnt/{HUMAN_REFERENCE} \\\n'.format(HUMAN_REFERENCE=input_parameters['genome_reference']) )
+        out.write( '/mnt/{TBAM} \\\n'.format(TBAM=input_parameters['tumor_bam']) )
+        out.write( '/mnt/{NBAM}\n\n'.format(NBAM=input_parameters['normal_bam']) )
+        
+        out.write( 'docker run --rm -v /:/mnt -u $UID --memory {}G marghoob/muse:1.0rc_c \\\n'.format(mem) )
+        out.write( 'MuSEv1.0rc_submission_c039ffa sump \\\n' )
+        out.write( '-I /mnt/{OUTDIR}/MuSE.MuSE.txt \\\n'.format(OUTDIR=input_parameters['output_directory']) )
+        out.write( '{statFlag} {EXTRA_ARGS} -O /mnt/{OUTDIR}/{OUTVCF} \\\n'.format(statFlag=statFlag, EXTRA_ARGS=input_parameters['muse_arguments'], OUTDIR=input_parameters['output_directory'], OUTVCF=outvcf) )
+        out.write( '-D /mnt/{DBSNP}.gz\n'.format(DBSNP=input_parameters['dbsnp_vcf']) )
+    
+        out.write( '\necho -e "Done at `date +"%Y/%m/%d %H:%M:%S"`" 1>&2\n' )
+        
+    returnCode = os.system('{} {}'.format(input_parameters['action'], outfile) )
+
+    return returnCode
+
+
+
+def run_Strelka2(input_parameters, mem=4, outdirname='Strelka'):
+
+    logdir         = input_parameters['output_directory'] + os.sep + 'logs'
+    outfile        = logdir + os.sep + 'strelka.{}.cmd'.format(ts)
+    
+    exomeFlag = '--exome' if input_parameters['exome_setting'] else ''
+    bed_gz = os.path.basename(input_parameters['inclusion_region']) + '.gz'
+    
+    with open(outfile, 'w') as out:
+
+        out.write( "#!/bin/bash\n\n" )
+        
+        out.write( '#$ -o {LOGDIR}\n'.format(LOGDIR=logdir) )
+        out.write( '#$ -e {LOGDIR}\n'.format(LOGDIR=logdir) )
+        out.write( '#$ -S /bin/bash\n' )
+        out.write( '#$ -l h_vmem={}G\n'.format(mem) )
+        out.write( 'set -e\n\n' )
+        
+        out.write( 'echo -e "Start at `date +"%Y/%m/%d %H:%M:%S"`" 1>&2\n\n' )
+
+        out.write( 'docker run -v /:/mnt -u $UID --rm --memory {MEM}G lethalfang/tabix:1.7 bash -c "cat /mnt/{SELECTOR} | bgzip > /mnt/{OUTDIR}/{BEDGZ}\"\n'.format(MEM=mem, SELECTOR=input_parameters['inclusion_region'], OUTDIR=input_parameters['output_directory'], BEDGZ=bed_gz ) )
+        out.write( 'docker run -v /:/mnt -u $UID --rm --memory {MEM}G lethalfang/tabix:1.7 tabix -f /mnt/{OUTDIR}/{BEDGZ}\n\n'.format(MEM=mem, OUTDIR=input_parameters['output_directory'], BEDGZ=bed_gz ) )
+
+        out.write( 'docker run --rm -v /:/mnt -u $UID --memory {MEM}G lethalfang/strelka:2.9.5 \\\n'.format(MEM=mem) )
+        out.write( '/opt/strelka/bin/configureStrelkaSomaticWorkflow.py \\\n' )
+        out.write( '--tumorBam=/mnt/{TBAM} \\\n'.format( TBAM=input_parameters['tumor_bam'] ) )
+        out.write( '--normalBam=/mnt/{NBAM} \\\n'.format( NBAM=input_parameters['normal_bam'] ) )
+        out.write( '--referenceFasta=/mnt/{HUMAN_REFERENCE} \\\n'.format( HUMAN_REFERENCE=input_parameters['genome_reference'] ) )
+        out.write( '--callMemMb={MEM} \\\n'.format(MEM=mem*1024) )
+        out.write( '--callRegions=/mnt/{BEDGZ} \\\n'.format(BEDGZ=bed_gz) )
+        out.write( '{EXOMEFLAG} {EXTRA_ARGS} \\\n'.format(EXOMEFLAG=exomeFlag, EXTRA_ARGS=input_parameters['strelka_config_arguments']) )
+        out.write( '--runDir=/mnt/{OUTDIR}/{DIRNAME}\n\n'.format(OUTDIR=input_parameters['output_directory'], DIRNAME=outdirname) )        
+        
+        out.write( 'docker run --rm -v /:/mnt -u $UID --memory {MEM}G lethalfang/strelka:2.9.5 \\\n'.format(MEM=mem) )
+        out.write( '/mnt/{OUTDIR}/{DIRNAME}/runWorkflow.py -m local -j 1 {EXTRA_ARGS}\n'.format(OUTDIR=input_parameters['output_directory'], DIRNAME=outdirname, EXTRA_ARGS=input_parameters['strelka_run_arguments']) )
 
         out.write( '\necho -e "Done at `date +"%Y/%m/%d %H:%M:%S"`" 1>&2\n' )
         
@@ -416,11 +499,24 @@ def run_VarDict(input_parameters, mem=14, minVAF=0.05, outvcf='VarDict.vcf'):
 
 
 
+
+
+
+
+##########################################################
+
 if __name__ == '__main__':
     
     workflowArguments = run()
     
-    subBeds = splitRegions(workflowArguments['threads'], workflowArguments['output_directory'] + os.sep + 'bed', bed=workflowArguments['inclusion_region'], fai=workflowArguments['genome_reference']+'.fai')
+    if workflowArguments['inclusion_region']:
+        bed_file = workflowArguments['inclusion_region']
+        
+    else:
+        split_bed.fai2bed(workflowArguments['genome_reference'] + '.fai', workflowArguments['output_directory'] + os.sep + 'genome.bed')
+        bed_file = workflowArguments['output_directory'] + os.sep + 'genome.bed'
+    
+    split_bed.split(bed_file, workflowArguments['output_directory'] + os.sep + 'bed', workflowArguments['threads'])
 
     os.makedirs(workflowArguments['output_directory'] + os.sep + 'logs', exist_ok=True)
 
@@ -448,9 +544,8 @@ if __name__ == '__main__':
             move(workflowArguments['output_directory'] + os.sep + str(thread_i) + '.bed', perThreadParameter['output_directory'])
         
         else:
-            perThreadParameter = workflowArguments
-        
-        
+            perThreadParameter = copy(workflowArguments)
+            perThreadParameter['inclusion_region'] = bed_file
         
         # Invoke parallelizable callers one by one:
         if workflowArguments['run_mutect2']:
@@ -459,6 +554,12 @@ if __name__ == '__main__':
         if workflowArguments['run_varscan2']:
             run_VarScan2( perThreadParameter )
             
-            
-            
-            
+        if workflowArguments['run_vardict']:
+            run_VarDict( perThreadParameter )
+
+        if workflowArguments['run_muse']:
+            run_MuSE( perThreadParameter )
+
+        if workflowArguments['run_strelka2']:
+            run_Strelka2( perThreadParameter )
+
