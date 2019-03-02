f9806a168ba89da569cf93fe12a5ac4a42491b03
Li Tai Fang <li_tai.fang@bina.roche.com>
2019-03-02 13:53:23 -0800

    .

--- utilities/neusomatic/modify_confLabel.py
+++ utilities/neusomatic/modify_confLabel.py
@@ -14,19 +14,21 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 parser.add_argument('-original',  '--original-vcf',        type=str, help='VCF in',  required=True)
 parser.add_argument('-mod',       '--modifier',            type=str, help='TSV in',  required=True)
 parser.add_argument('-outfile',   '--outfile',             type=str, help='VCF out', required=True)
+parser.add_argument('--promote-long-deletions',  action='store_true', help='Up MedConf >=15-bp indels to HighConf')
 
 
 args = parser.parse_args()
 
-originalFile = args.original_vcf
-modifiers    = args.modifier
-outfile      = args.outfile
-
+originalFile      = args.original_vcf
+modifiers         = args.modifier
+outfile           = args.outfile
+promote_long_dels = args.promote_long_deletions
+len_long_del      = 16
 
 mod = {}
 labelMods = pd.ExcelFile(modifiers)
 
-for sheet_i in ('HighConf NeuCall<=21', 'MedConf NeuCall<=10', 'Unclassified NeuCall majority', 'InDel HighConf Neu<10', 'InDel MedConf Neu<10', 'InDel Unclassified Neu Majority'):
+for sheet_i in ('HighConf NeuCall<=21', 'MedConf NeuCall<=10', 'Unclassified NeuCall majority', 'InDel HighConf Neu<=21', 'InDel MedConf Neu<=10', 'InDel Unclassified Neu Majority'):
 
     sheet = labelMods.parse(sheet_i)
 
@@ -34,7 +36,6 @@ for sheet_i in ('HighConf NeuCall<=21', 'MedConf NeuCall<=10', 'Unclassified Neu
         if 'NO CHANGE' not in row['Label Change']:
             variant_i = row['CHROM'], int( row['POS'] ), row['REF'], row['ALT']
             mod[ variant_i ] = row['Label Change']
-
 
 
 
@@ -57,6 +58,13 @@ with genome.open_textfile(originalFile) as original, open(outfile, 'w') as out:
             item = line_i.split('\t')
             item[6] = conf_level
             line_i = '\t'.join(item)
+            
+        elif promote_long_dels:
+            if (len(vcf_i.refbase)>=len_long_del) and (len(vcf_i.altbase) == 1) and ('MedConf' in vcf_i.filters):
+                conf_level = re.sub(r'MedConf', 'HighConf', vcf_i.filters)
+                item = line_i.split('\t')
+                item[6] = conf_level
+                line_i = '\t'.join(item)
         
         out.write( line_i + '\n' )
         line_i = original.readline().rstrip()
