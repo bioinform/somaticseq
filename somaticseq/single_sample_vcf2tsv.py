566707c7d2cab9028bf2e3f7bfac599c06f635dd
Li Tai Fang <li_tai.fang@roche.com>
2020-03-14 14:57:59 -0700

    remove some relative paths

--- somaticseq/single_sample_vcf2tsv.py
+++ somaticseq/single_sample_vcf2tsv.py
@@ -4,9 +4,9 @@
 
 import sys, argparse, math, gzip, os, pysam, re, logging
 
-MY_DIR = os.path.dirname(os.path.realpath(__file__))
-PRE_DIR = os.path.join(MY_DIR, os.pardir)
-sys.path.append( PRE_DIR )
+#MY_DIR = os.path.dirname(os.path.realpath(__file__))
+#PRE_DIR = os.path.join(MY_DIR, os.pardir)
+#sys.path.append( PRE_DIR )
 
 import scipy.stats as stats
 from copy import copy
