9fbdb1e55ed4b7657096f8e2d66e95f71bf9af21
Li Tai Fang <li_tai.fang@roche.com>
2020-03-21 15:51:09 -0700

    linguistic sequence complexity

--- utilities/linguistic_sequence_complexity.py
+++ utilities/linguistic_sequence_complexity.py
@@ -35,13 +35,34 @@ def max_vocab(seq_length):
 
 
 
+
+
+def max_vocabularies(seq_length):
+    # According to:
+    # https://doi.org/10.1093/bioinformatics/18.5.679
+    counts = 0
+    k = 1
+    while k <= seq_length:
+        
+        if 4**k < (seq_length - k + 1):
+            counts = counts + 4**k
+        else:
+            counts = counts + (seq_length-k+1 + 1) * (seq_length-k+1 - 1 + 1)/2
+            break
+        
+        k += 1
+                
+    return counts
+
+
+
+
 def LC(sequence):
     # Calculate linguistic sequence complexity according to
     # https://doi.org/10.1093/bioinformatics/18.5.679
-    
     number_of_subseqs     = 0
     seq_length            = len(sequence)
-    max_number_of_subseqs = max_vocab(seq_length)
+    max_number_of_subseqs = max_vocabularies(seq_length)
 
     for i in range(1, seq_length+1):
         
@@ -52,18 +73,20 @@ def LC(sequence):
                 sub_seq = sequence[n:n+i]
                 set_of_seq_n.add( sub_seq )
 
+                # All possible unique subseqs obtained. Break away and go no further. 
+                if ( len(set_of_seq_n) == seq_length-i+1 ) and ( 4**i >= (seq_length - i + 1) ):
+                    break
+
         num_uniq_subseqs  = len(set_of_seq_n)
         number_of_subseqs = number_of_subseqs + num_uniq_subseqs
-        
 
     return number_of_subseqs/max_number_of_subseqs
 
 
 
 
-
 '''
-def linguistic_sequence_complexity(sequence, up_to_n=50, at_least_to_n=10):
+def LSC(sequence, up_to_n=50, at_least_to_n=10):
     #Calculate the number of unique N-string within a sequence divided by min(4^i or N-i+1) as U(i).
     #Then, take the product of all the U(i)'s.
     
