cd790aa647a3418c7231fdec15b8ab641d5aa22d
Li Tai Fang <li_tai.fang@roche.com>
2020-03-23 12:37:15 -0700

    put LC function to somaticseq.sequencing_features

--- utilities/linguistic_sequence_complexity.py
+++ utilities/linguistic_sequence_complexity.py
@@ -3,6 +3,7 @@
 from copy import copy
 from sys import float_info
 import argparse
+import somaticseq.sequencing_features as seq_features
 
 eps = float_info.epsilon
 
@@ -35,55 +36,6 @@ def max_vocab(seq_length):
     
     return counts
 
-
-
-def max_vocabularies(seq_length):
-    # According to:
-    # https://doi.org/10.1093/bioinformatics/18.5.679
-    counts = 0
-    k = 1
-    while k <= seq_length:
-        
-        if 4**k < (seq_length - k + 1):
-            counts = counts + 4**k
-        else:
-            counts = counts + (seq_length-k+1 + 1) * (seq_length-k+1 - 1 + 1)/2
-            break
-        
-        k += 1
-                
-    return counts
-
-
-
-def LC(sequence):
-    # Calculate linguistic sequence complexity according to
-    # https://doi.org/10.1093/bioinformatics/18.5.679
-    sequence              = sequence.upper()
-    number_of_subseqs     = 0
-    seq_length            = len(sequence)
-    max_number_of_subseqs = max_vocabularies(seq_length)
-
-    for i in range(1, seq_length+1):
-        
-        max_vocab_1 = 4**i
-        max_vocab_2 = seq_length - i + 1
-        set_of_seq_n = set()
-
-        for n, nth_base in enumerate(sequence):
-            
-            if n+i <= len(sequence):
-                sub_seq = sequence[n:n+i]
-                set_of_seq_n.add( sub_seq )
-
-                # All possible unique subseqs obtained. Break away and go no further. 
-                if ( len(set_of_seq_n) == max_vocab_2 ) and ( max_vocab_1 >= max_vocab_2 ):
-                    break
-
-        num_uniq_subseqs  = len(set_of_seq_n)
-        number_of_subseqs = number_of_subseqs + num_uniq_subseqs
-
-    return number_of_subseqs/max_number_of_subseqs
 
 
 
@@ -130,4 +82,4 @@ if __name__ == "__main__":
     parser.add_argument('-seq',  '--sequence', type=str,  help="input vcf file")
     args = parser.parse_args()
 
-    print( LC(args.sequence) )
+    print( seq_features.LC(args.sequence) )
