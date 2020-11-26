da6069b7fffc54839d6a2ef371bfa6fef5448f7d
Li Tai Fang <ltfang@gmail.com>
2020-11-26 12:47:16 -0800

    maintenance

--- example/invoke_dockerized_tumor_normal_callers.sh
+++ example/invoke_dockerized_tumor_normal_callers.sh
@@ -11,4 +11,4 @@ paired \
 --truth-snv        $(pwd -P)/Varsim.somatic.truth.vcf \
 --truth-indel      $(pwd -P)/Varsim.somatic.truth.vcf \
 --dbsnp-vcf        $(pwd -P)/tiny_dbsnp.vcf \
---run-mutect2 --run-vardict --run-strelka2 --run-somaticseq --train-somaticseq --run-workflow -nt 2 --run-workflow
+--run-mutect2 --run-vardict --run-strelka2 --run-somaticseq --train-somaticseq -nt 2 --run-workflow
