4d4b9b4554a4e434c8f19f855ae94932c3db099a
Li Tai Fang <ltfang@gmail.com>
2022-04-14 15:53:35 -0700

    example with extra-hyperparameters

--- example/paired_somaticseq_example.sh
+++ example/paired_somaticseq_example.sh
@@ -5,18 +5,19 @@ set -e
 mkdir -p paired_somaticseq
 
 somaticseq_parallel.py \
---somaticseq-train \
---algorithm        xgboost \
---output-directory paired_somaticseq \
---genome-reference tiny.fa \
---dbsnp-vcf        tiny_dbsnp.vcf \
---truth-snv        Varsim.somatic.truth.vcf \
---truth-indel      Varsim.somatic.truth.vcf \
---threads          3 \
+--somaticseq-train      \
+--algorithm             xgboost \
+--extra-hyperparameters scale_pos_weight:0.1 seed:100 \
+--output-directory      paired_somaticseq \
+--genome-reference      tiny.fa \
+--dbsnp-vcf             tiny_dbsnp.vcf \
+--truth-snv             Varsim.somatic.truth.vcf \
+--truth-indel           Varsim.somatic.truth.vcf \
+--threads               3 \
 paired \
---tumor-bam-file   tumor.markdup.bam \
---normal-bam-file  normal.markdup.bam \
---mutect2-vcf      paired_example/MuTect2.vcf.gz \
---vardict-vcf      paired_example/VarDict.vcf.gz \
---strelka-snv      paired_example/Strelka/results/variants/somatic.snvs.vcf.gz \
---strelka-indel    paired_example/Strelka/results/variants/somatic.indels.vcf.gz
+--tumor-bam-file        tumor.markdup.bam \
+--normal-bam-file       normal.markdup.bam \
+--mutect2-vcf           paired_example/MuTect2.vcf.gz \
+--vardict-vcf           paired_example/VarDict.vcf.gz \
+--strelka-snv           paired_example/Strelka/results/variants/somatic.snvs.vcf.gz \
+--strelka-indel         paired_example/Strelka/results/variants/somatic.indels.vcf.gz
--- example/single_somaticseq_example.sh
+++ example/single_somaticseq_example.sh
@@ -6,15 +6,16 @@ mkdir -p single_somaticseq
 
 somaticseq_parallel.py \
 --somaticseq-train \
---algorithm        xgboost \
---output-directory single_somaticseq \
---genome-reference tiny.fa \
---dbsnp-vcf        tiny_dbsnp.vcf \
---truth-snv        Varsim.somatic.truth.vcf \
---truth-indel      Varsim.somatic.truth.vcf \
---threads          3 \
+--algorithm             xgboost \
+--extra-hyperparameters scale_pos_weight:0.1 seed:100 \
+--output-directory      single_somaticseq \
+--genome-reference      tiny.fa \
+--dbsnp-vcf             tiny_dbsnp.vcf \
+--truth-snv             Varsim.somatic.truth.vcf \
+--truth-indel           Varsim.somatic.truth.vcf \
+--threads               3 \
 single \
---bam-file         tumor.markdup.bam \
---mutect2-vcf      paired_example/MuTect2.vcf.gz \
---vardict-vcf      paired_example/VarDict.vcf.gz \
---strelka-vcf      tumor_only_example/Strelka/results/variants/variants.vcf.gz
+--bam-file              tumor.markdup.bam \
+--mutect2-vcf           paired_example/MuTect2.vcf.gz \
+--vardict-vcf           paired_example/VarDict.vcf.gz \
+--strelka-vcf           tumor_only_example/Strelka/results/variants/variants.vcf.gz
