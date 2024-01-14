7ce8b96f49c2300863cd1b0aa7cab8e2545ad22c
Li Tai Fang <litai.fang@freenome.com>
2024-01-14 10:17:57 -0800

    wip

--- somaticseq/bam_features.py
+++ somaticseq/bam_features.py
@@ -1,5 +1,6 @@
 import pysam
 import scipy.stats as stats
+from functools import cached_property
 from pydantic import BaseModel
 
 from somaticseq.genomicFileHandler.read_info_extractor import (
@@ -13,45 +14,45 @@ nan = float("nan")
 
 
 class BamFeatures(BaseModel):
-    dp: int
-    ref_call_forward: int
-    ref_call_reverse: int
-    alt_call_forward: int
-    alt_call_reverse: int
-    consistent_mates: int
-    inconsistent_mates: int
-    ref_mq: float
-    alt_mq: float
-    p_mannwhitneyu_mq: float
-    ref_bq: int
-    alt_bq: int
-    p_mannwhitneyu_bq: float
-    ref_edit_distance: int
-    alt_edit_distance: int
-    edit_distance_difference: int
-    ref_concordant_reads: int
-    ref_discordant_reads: int
-    alt_concordant_reads: int
-    alt_discordant_reads: int
-    concordance_fet: float
-    strandbias_fet: float
-    ref_SC_reads: int
-    alt_SC_reads: int
-    clipping_fet: float
-    p_mannwhitneyu_endpos: float
-    mq0_reads: int
-    noise_read_count: int
-    poor_read_count: int
-    ref_indel_3bp: int
-    ref_indel_2bp: int
-    ref_indel_1bp: int
-    alt_indel_3bp: int
-    alt_indel_2bp: int
-    alt_indel_1bp: int
-    indel_length: int
+    dp: int = 0
+    ref_call_forward: int = 0
+    ref_call_reverse: int = 0
+    alt_call_forward: int = 0
+    alt_call_reverse: int = 0
+    consistent_mates: int = 0
+    inconsistent_mates: int = 0
+    ref_mq: float = nan
+    alt_mq: float = nan
+    p_mannwhitneyu_mq: float = nan
+    ref_bq: float = nan
+    alt_bq: float = nan
+    p_mannwhitneyu_bq: float = nan
+    ref_edit_distance: int = 0
+    alt_edit_distance: int = 0
+    edit_distance_difference: int = 0
+    ref_concordant_reads: int = 0
+    ref_discordant_reads: int = 0
+    alt_concordant_reads: int = 0
+    alt_discordant_reads: int = 0
+    concordance_fet: float = nan
+    strandbias_fet: float = nan
+    ref_soft_clipped_reads: int = 0
+    alt_soft_clipped_reads: int = 0
+    clipping_fet: float = nan
+    p_mannwhitneyu_endpos: float = nan
+    mq0_reads: int = 0
+    noise_read_count: int = 0
+    poor_read_count: int = 0
+    ref_indel_3bp: int = 0
+    ref_indel_2bp: int = 0
+    ref_indel_1bp: int = 0
+    alt_indel_3bp: int = 0
+    alt_indel_2bp: int = 0
+    alt_indel_1bp: int = 0
+    indel_length: int = 0
 
     @classmethod
-    def from_pysam_record(
+    def from_alignment_file(
         cls,
         bam_fh: pysam.AlignmentFile,
         my_coordinate: tuple[str, int, int],
@@ -60,96 +61,102 @@ class BamFeatures(BaseModel):
         min_mq: int = 1,
         min_bq: int = 10,
     ) -> BaseModel:
+        
         indel_length = len(first_alt) - len(ref_base)
-        reads = bam_fh.fetch(my_coordinate[0], my_coordinate[1] - 1, my_coordinate[1])
 
+        reads = bam_fh.fetch(my_coordinate[0], my_coordinate[1] - 1, my_coordinate[1])
         ref_read_mq = []
         alt_read_mq = []
         ref_read_bq = []
         alt_read_bq = []
         ref_edit_distance = []
         alt_edit_distance = []
-
-        ref_concordant_reads = (
-            alt_concordant_reads
-        ) = ref_discordant_reads = alt_discordant_reads = 0
-        ref_for = ref_rev = alt_for = alt_rev = dp = 0
-        ref_SC_reads = alt_SC_reads = ref_notSC_reads = alt_notSC_reads = 0
-        MQ0 = 0
-
         ref_pos_from_end = []
         alt_pos_from_end = []
         ref_flanking_indel = []
         alt_flanking_indel = []
-
-        noise_read_count = poor_read_count = 0
-
+        ref_concordant_reads = 0
+        alt_concordant_reads = 0
+        ref_discordant_reads = 0
+        alt_discordant_reads = 0
+        ref_for = 0
+        ref_rev = 0
+        alt_for = 0
+        alt_rev = 0
+        dp = 0
+        ref_SC_reads = 0
+        alt_SC_reads = 0
+        ref_notSC_reads = 0
+        alt_notSC_reads = 0
+        mq0_reads = 0
+        noise_read_count = 0
+        poor_read_count = 0
         qname_collector: dict[str, list[int]] = {}
-        for read_i in reads:
-            if not read_i.is_unmapped and dedup_test(read_i):
+        for read in reads:
+            if not read.is_unmapped and dedup_test(read):
                 dp += 1
                 (
-                    code_i,
+                    call_code,
                     ith_base,
                     base_call_i,
                     indel_length_i,
                     flanking_indel_i,
-                ) = position_of_aligned_read(read_i, my_coordinate[1] - 1)
+                ) = position_of_aligned_read(read, my_coordinate[1] - 1)
                 if (
-                    read_i.mapping_quality < min_mq
-                    and mean(read_i.query_qualities) < min_bq
+                    read.mapping_quality < min_mq
+                    and mean(read.query_qualities) < min_bq
                 ):
                     poor_read_count += 1
-                if read_i.mapping_quality == 0:
-                    MQ0 += 1
+                if read.mapping_quality == 0:
+                    mq0_reads += 1
 
                 # Reference calls:
-                if code_i == 1 and base_call_i == ref_base[0]:
+                if call_code == 1 and base_call_i == ref_base[0]:
                     try:
-                        qname_collector[read_i.query_name].append(0)
+                        qname_collector[read.query_name].append(0)
                     except KeyError:
-                        qname_collector[read_i.query_name] = [0]
+                        qname_collector[read.query_name] = [0]
 
-                    ref_read_mq.append(read_i.mapping_quality)
-                    ref_read_bq.append(read_i.query_qualities[ith_base])
+                    ref_read_mq.append(read.mapping_quality)
+                    ref_read_bq.append(read.query_qualities[ith_base])
 
                     try:
-                        ref_edit_distance.append(read_i.get_tag("NM"))
+                        ref_edit_distance.append(read.get_tag("NM"))
                     except KeyError:
                         pass
 
                     # Concordance
                     if (
-                        read_i.is_proper_pair
-                        and read_i.mapping_quality >= min_mq
-                        and read_i.query_qualities[ith_base] >= min_bq
+                        read.is_proper_pair
+                        and read.mapping_quality >= min_mq
+                        and read.query_qualities[ith_base] >= min_bq
                     ):
                         ref_concordant_reads += 1
                     elif (
-                        (not read_i.is_proper_pair)
-                        and read_i.mapping_quality >= min_mq
-                        and read_i.query_qualities[ith_base] >= min_bq
+                        (not read.is_proper_pair)
+                        and read.mapping_quality >= min_mq
+                        and read.query_qualities[ith_base] >= min_bq
                     ):
                         ref_discordant_reads += 1
 
                     # Orientation
                     if (
-                        (not read_i.is_reverse)
-                        and read_i.mapping_quality >= min_mq
-                        and read_i.query_qualities[ith_base] >= min_bq
+                        (not read.is_reverse)
+                        and read.mapping_quality >= min_mq
+                        and read.query_qualities[ith_base] >= min_bq
                     ):
                         ref_for += 1
                     elif (
-                        read_i.is_reverse
-                        and read_i.mapping_quality >= min_mq
-                        and read_i.query_qualities[ith_base] >= min_bq
+                        read.is_reverse
+                        and read.mapping_quality >= min_mq
+                        and read.query_qualities[ith_base] >= min_bq
                     ):
                         ref_rev += 1
 
                     # Soft-clipped reads?
                     if (
-                        read_i.cigar[0][0] == CIGAR_SOFT_CLIP
-                        or read_i.cigar[-1][0] == CIGAR_SOFT_CLIP
+                        read.cigar[0][0] == CIGAR_SOFT_CLIP
+                        or read.cigar[-1][0] == CIGAR_SOFT_CLIP
                     ):
                         ref_SC_reads += 1
                     else:
@@ -158,7 +165,7 @@ class BamFeatures(BaseModel):
                     # Distance from the end of the read:
                     if ith_base != None:
                         ref_pos_from_end.append(
-                            min(ith_base, read_i.query_length - ith_base)
+                            min(ith_base, read.query_length - ith_base)
                         )
 
                     # Flanking indels:
@@ -167,59 +174,59 @@ class BamFeatures(BaseModel):
                 # Alternate calls:
                 # SNV, or Deletion, or Insertion where I do not check for matching indel length
                 elif (
-                    (indel_length == 0 and code_i == 1 and base_call_i == first_alt)
+                    (indel_length == 0 and call_code == 1 and base_call_i == first_alt)
                     or (
                         indel_length < 0
-                        and code_i == 2
+                        and call_code == 2
                         and indel_length == indel_length_i
                     )
-                    or (indel_length > 0 and code_i == 3)
+                    or (indel_length > 0 and call_code == 3)
                 ):
                     try:
-                        qname_collector[read_i.query_name].append(1)
+                        qname_collector[read.query_name].append(1)
                     except KeyError:
-                        qname_collector[read_i.query_name] = [1]
+                        qname_collector[read.query_name] = [1]
 
-                    alt_read_mq.append(read_i.mapping_quality)
-                    alt_read_bq.append(read_i.query_qualities[ith_base])
+                    alt_read_mq.append(read.mapping_quality)
+                    alt_read_bq.append(read.query_qualities[ith_base])
 
                     try:
-                        alt_edit_distance.append(read_i.get_tag("NM"))
+                        alt_edit_distance.append(read.get_tag("NM"))
                     except KeyError:
                         pass
 
                     # Concordance
                     if (
-                        read_i.is_proper_pair
-                        and read_i.mapping_quality >= min_mq
-                        and read_i.query_qualities[ith_base] >= min_bq
+                        read.is_proper_pair
+                        and read.mapping_quality >= min_mq
+                        and read.query_qualities[ith_base] >= min_bq
                     ):
                         alt_concordant_reads += 1
                     elif (
-                        (not read_i.is_proper_pair)
-                        and read_i.mapping_quality >= min_mq
-                        and read_i.query_qualities[ith_base] >= min_bq
+                        (not read.is_proper_pair)
+                        and read.mapping_quality >= min_mq
+                        and read.query_qualities[ith_base] >= min_bq
                     ):
                         alt_discordant_reads += 1
 
                     # Orientation
                     if (
-                        (not read_i.is_reverse)
-                        and read_i.mapping_quality >= min_mq
-                        and read_i.query_qualities[ith_base] >= min_bq
+                        (not read.is_reverse)
+                        and read.mapping_quality >= min_mq
+                        and read.query_qualities[ith_base] >= min_bq
                     ):
                         alt_for += 1
                     elif (
-                        read_i.is_reverse
-                        and read_i.mapping_quality >= min_mq
-                        and read_i.query_qualities[ith_base] >= min_bq
+                        read.is_reverse
+                        and read.mapping_quality >= min_mq
+                        and read.query_qualities[ith_base] >= min_bq
                     ):
                         alt_rev += 1
 
                     # Soft-clipped reads?
                     if (
-                        read_i.cigar[0][0] == CIGAR_SOFT_CLIP
-                        or read_i.cigar[-1][0] == CIGAR_SOFT_CLIP
+                        read.cigar[0][0] == CIGAR_SOFT_CLIP
+                        or read.cigar[-1][0] == CIGAR_SOFT_CLIP
                     ):
                         alt_SC_reads += 1
                     else:
@@ -228,7 +235,7 @@ class BamFeatures(BaseModel):
                     # Distance from the end of the read:
                     if ith_base != None:
                         alt_pos_from_end.append(
-                            min(ith_base, read_i.query_length - ith_base)
+                            min(ith_base, read.query_length - ith_base)
                         )
 
                     # Flanking indels:
@@ -237,9 +244,9 @@ class BamFeatures(BaseModel):
                 # Inconsistent read or 2nd alternate calls:
                 else:
                     try:
-                        qname_collector[read_i.query_name].append(2)
+                        qname_collector[read.query_name].append(2)
                     except KeyError:
-                        qname_collector[read_i.query_name] = [2]
+                        qname_collector[read.query_name] = [2]
 
                     noise_read_count += 1
 
@@ -335,11 +342,11 @@ class BamFeatures(BaseModel):
             alt_discordant_reads=alt_discordant_reads,
             concordance_fet=concordance_fet,
             strandbias_fet=strandbias_fet,
-            ref_SC_reads=ref_SC_reads,
-            alt_SC_reads=alt_SC_reads,
+            ref_soft_clipped_reads=ref_SC_reads,
+            alt_soft_clipped_reads=alt_SC_reads,
             clipping_fet=clipping_fet,
             p_mannwhitneyu_endpos=p_mannwhitneyu_endpos,
-            mq0_reads=MQ0,
+            mq0_reads=mq0_reads,
             noise_read_count=noise_read_count,
             poor_read_count=poor_read_count,
             ref_indel_3bp=ref_indel_3bp,
