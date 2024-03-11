61bd5c37be15bb91e573234f2836d9490a5d8435
Li Tai Fang <ltfang@gmail.com>
2024-03-10 23:41:07 -0700

    generalize test examples

--- example/invoke_dockerized_tumor_normal_callers.sh
+++ example/invoke_dockerized_tumor_normal_callers.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+MYDIR="$( cd "$( dirname "$0" )" && pwd )"
+
 mkdir -p paired_example
 
 makeSomaticScripts.py \
 paired \
 --output-directory $(pwd -P)/paired_example \
---tumor-bam        $(pwd -P)/tumor.markdup.bam \
---normal-bam       $(pwd -P)/normal.markdup.bam \
---genome-reference $(pwd -P)/tiny.fa \
---truth-snv        $(pwd -P)/Varsim.somatic.truth.vcf \
---truth-indel      $(pwd -P)/Varsim.somatic.truth.vcf \
---dbsnp-vcf        $(pwd -P)/tiny_dbsnp.vcf \
+--tumor-bam        ${MYDIR}/tumor.markdup.bam \
+--normal-bam       ${MYDIR}/normal.markdup.bam \
+--genome-reference ${MYDIR}/tiny.fa \
+--truth-snv        ${MYDIR}/Varsim.somatic.truth.vcf \
+--truth-indel      ${MYDIR}/Varsim.somatic.truth.vcf \
+--dbsnp-vcf        ${MYDIR}/tiny_dbsnp.vcf \
 --run-mutect2 --run-somaticsniper --run-vardict --run-muse --run-lofreq --run-scalpel --run-strelka2 \
 --run-somaticseq --train-somaticseq \
 --threads 2 --run-workflow
--- example/invoke_dockerized_tumor_only_callers.sh
+++ example/invoke_dockerized_tumor_only_callers.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+MYDIR="$( cd "$( dirname "$0" )" && pwd )"
+
 mkdir -p tumor_only_example
 
 makeSomaticScripts.py \
 single \
 --output-directory $(pwd -P)/tumor_only_example \
---bam              $(pwd -P)/tumor.markdup.bam \
---genome-reference $(pwd -P)/tiny.fa \
---truth-snv        $(pwd -P)/Varsim.somatic.truth.vcf \
---truth-indel      $(pwd -P)/Varsim.somatic.truth.vcf \
---dbsnp-vcf        $(pwd -P)/tiny_dbsnp.vcf \
+--bam              ${MYDIR}/tumor.markdup.bam \
+--genome-reference ${MYDIR}/tiny.fa \
+--truth-snv        ${MYDIR}/Varsim.somatic.truth.vcf \
+--truth-indel      ${MYDIR}/Varsim.somatic.truth.vcf \
+--dbsnp-vcf        ${MYDIR}/tiny_dbsnp.vcf \
 --run-mutect2 --run-vardict --run-strelka2 --run-somaticseq --train-somaticseq -nt 2 --run-workflow
