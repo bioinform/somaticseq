71b31588930e2d490aef4d9816ba1b2e6c7c9bea
Li Tai Fang <ltfang@gmail.com>
2025-01-05 00:12:31 +0000

    modify description string in setup.py to convert relative links to absolute links

--- setup.py
+++ setup.py
@@ -1,12 +1,45 @@
 #!/usr/bin/env python3
 # mypy: ignore-errors
 import os
+import re
 
 import setuptools_scm  # noqa
 from setuptools import find_packages, setup
 
+LINK_PATTERN = r"(\[.*?\]\()([^http][^)]*)\)"
+IMAGE_SRC_PATTERN = r'(<img\s+[^>]*src=")([^"]*)(")'
+BASE_URL = "https://github.com/bioinform/somaticseq"
+
+
+def replace_link(match: re.Match) -> str:
+    """
+    Replace relative links in .md from [text](RELATIVE/LINK) into
+    [text]({BASE_URL}/blob/master/RELATIVE/LINK)
+    """
+    text = match.group(1)
+    url = match.group(2)
+    return f"{text}{BASE_URL}/blob/master/{url})"
+
+
+def replace_src(match: re.Match) -> str:
+    """
+    Replace relative image links like above
+    """
+    prefix = match.group(1)  # The part before the URL
+    url = match.group(2)  # The original URL
+    suffix = match.group(3)  # The part after the URL
+    return f"{prefix}{BASE_URL}/raw/master/{url}{suffix}"
+
+
+def modify_markdown_for_3rd_party(base_markdown: str) -> str:
+    with_abs_url = re.sub(LINK_PATTERN, replace_link, base_markdown)
+    with_abs_img_src = re.sub(IMAGE_SRC_PATTERN, replace_src, with_abs_url)
+    return with_abs_img_src
+
+
 with open("README.md") as fn:
     long_description = fn.read()
+    description_for_3rd_party = modify_markdown_for_3rd_party(long_description)
 
 # Read the version from the _version.py file
 version_file = os.path.join("somaticseq", "_version.py")
@@ -20,7 +53,7 @@ setup(
         "An ensemble approach to accurately detect somatic mutations using SomaticSeq"
     ),
     version=__version__,  # noqa
-    long_description=long_description,
+    long_description=description_for_3rd_party,
     long_description_content_type="text/markdown",
     author="Li Tai Fang",
     author_email="ltfang@gmail.com",
@@ -29,7 +62,7 @@ setup(
     package_data={"": ["*.R"]},
     python_requires=">=3.11.0",
     setup_requires=["setuptools>=42", "setuptools_scm"],
-    install_requires=[  # overridden by pyproject.toml
+    install_requires=[  # pyproject.toml overrides them
         "pysam",
         "numpy",
         "scipy",
