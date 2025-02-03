470dccb045827e0943159150e0b756eeb6db3628
Li Tai Fang <ltfang@gmail.com>
2025-02-03 09:15:31 +0000

    complete test

--- tests/unit/genomic_file_parsers/test_read_info_extractor.py
+++ tests/unit/genomic_file_parsers/test_read_info_extractor.py
@@ -34,8 +34,37 @@ def test_get_alignment_via_cigar() -> None:
     header_dict = {"SQ": [{"LN": 1, "SN": contig} for contig in ["chr1", "chr2"]]}
     header = pysam.AlignmentHeader.from_dict(header_dict)
     read = pysam.AlignedSegment.from_dict(read_dict, header)
-    for coordinate in range(100, 250):
+    for coordinate in range(300):
         seq_call = get_alignment_via_cigar(read, coordinate)
-        if coordinate in range(100, 195 + 1):
+        if coordinate in range(100, 195 + 1) or coordinate in (213, 214, 215):
             assert seq_call.call_type == AlignmentType.match
             assert seq_call.nearest_indel == float("inf")
+        elif coordinate in (196, 197, 198):
+            assert seq_call.call_type == AlignmentType.match
+            assert seq_call.nearest_indel == 199 - coordinate
+        elif coordinate == 199:
+            assert seq_call.call_type == AlignmentType.deletion
+            assert seq_call.indel_length == -10
+            assert seq_call.nearest_indel == float("inf")
+        elif coordinate in range(200, 210):
+            assert seq_call.call_type == AlignmentType.unknown
+        elif coordinate in (210, 211, 212):
+            assert seq_call.call_type == AlignmentType.match
+            assert seq_call.nearest_indel == coordinate - 209
+        elif coordinate in (216, 217, 218):
+            assert seq_call.call_type == AlignmentType.match
+            assert seq_call.nearest_indel == 219 - coordinate
+        elif coordinate == 219:
+            assert seq_call.call_type == AlignmentType.insertion
+            assert seq_call.indel_length == 10
+            assert seq_call.nearest_indel == float("inf")
+        elif coordinate == 229:
+            assert seq_call.call_type == AlignmentType.insertion
+            assert seq_call.indel_length == 5
+            assert seq_call.nearest_indel == float("inf")
+        elif coordinate == 234:
+            assert seq_call.call_type == AlignmentType.deletion
+            assert seq_call.indel_length == -5
+            assert seq_call.nearest_indel == float("inf")
+        elif 100 > coordinate >= 245:
+            assert seq_call.call_type is None
