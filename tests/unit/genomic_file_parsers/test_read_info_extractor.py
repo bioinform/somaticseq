bb7a70bc09fb74a3fe365ffdb01b75ca41494df5
Li Tai Fang <ltfang@gmail.com>
2025-02-07 21:17:10 +0000

    update test

--- tests/unit/genomic_file_parsers/test_read_info_extractor.py
+++ tests/unit/genomic_file_parsers/test_read_info_extractor.py
@@ -2,14 +2,13 @@ import pysam
 
 from somaticseq.genomic_file_parsers.read_info_extractor import (
     AlignmentType,
-    get_alignment_via_cigar,
+    get_alignment_in_read,
 )
 
 
 def test_get_alignment_via_cigar() -> None:
     """
-    Test the following aligned reads
-
+    Test the following aligned read:
     Coordinates:  100     200   210    220    230  235  240  245  250
                     ^      ^     ^      ^      ^    ^    ^    ^    ^
     Reference:      ================================================
@@ -19,24 +18,26 @@ def test_get_alignment_via_cigar() -> None:
     """
 
     read_dict = {
-        "name": "TEST_1",
-        "flag": "97",  # top read1
+        "name": "query_name",
+        "flag": "97",  # top strand read1
         "ref_name": "chr1",
-        "ref_pos": "101",  # convert to 1-based coordinate
+        "ref_pos": "101",  # 1-based coordinate
         "map_quality": "60",
         "cigar": "100M10D10M10I10M5I5M5D5M5S",
         "next_ref_name": "=",
-        "next_ref_pos": "250",  # convert to 1-based coordinate
-        "length": "150",
+        "next_ref_pos": "251",
+        "length": "300",  # template_length
         "seq": "A" * 150,
         "qual": "J" * 150,
     }
     header_dict = {"SQ": [{"LN": 1, "SN": contig} for contig in ["chr1", "chr2"]]}
     header = pysam.AlignmentHeader.from_dict(header_dict)
     read = pysam.AlignedSegment.from_dict(read_dict, header)
+    # matches that are more than 3 bps from the nearest indel
+    simple_matches = set(list(range(100, 195 + 1)) + [213, 214, 215, 223, 224, 225])
     for coordinate in range(300):
-        seq_call = get_alignment_via_cigar(read, coordinate)
-        if coordinate in range(100, 195 + 1) or coordinate in (213, 214, 215):
+        seq_call = get_alignment_in_read(read, coordinate)
+        if coordinate in simple_matches:
             assert seq_call.call_type == AlignmentType.match
             assert seq_call.nearest_indel == float("inf")
         elif coordinate in (196, 197, 198):
