c4b3814ca16d0b60cd2e12930b5e67c46a13e34a
Li Tai Fang <ltfang@gmail.com>
2025-02-13 22:03:26 +0000

    add a test and pytest-mock

---
+++ tests/unit/utilities/test_split_bed_into_equal_regions.py
@@ -0,0 +1,49 @@
+import os
+from collections.abc import Generator
+
+import pytest
+from _pytest.tmpdir import TempPathFactory
+from pytest_mock import MockerFixture
+
+from somaticseq.utilities.split_bed_into_equal_regions import split
+
+
+@pytest.mark.parametrize(
+    "expected_inlines,expected_outlines",
+    [
+        (
+            ["chr1\t0\t100\n", ""],
+            ["chr1\t0\t34\n", "chr1\t34\t68\n", "chr1\t68\t100\n"],
+        ),
+        (
+            ["chr1\t0\t90\n", "chr2\t0\t10\n", ""],
+            ["chr1\t0\t34\n", "chr1\t34\t68\n", "chr1\t68\t90\n", "chr2\t0\t10\n"],
+        ),
+    ],
+)
+def test_find_chunks_for_whole_bam(
+    expected_inlines: list[str],
+    expected_outlines: list[str],
+    mocker: MockerFixture,
+    tmp_path_factory: TempPathFactory,
+) -> None:
+
+    WRITTEN_LINES: list[str] = []
+
+    def _mock_reader(expected_inlines: list[str]) -> Generator:
+        yield from expected_inlines
+
+    def _mock_writer(line: str) -> None:
+        WRITTEN_LINES.append(line)
+
+    file_opener = mocker.MagicMock()
+    file_opener.readline.side_effect = _mock_reader(expected_inlines)
+    file_opener.write.side_effect = _mock_writer
+    mocked_fh = mocker.patch("somaticseq.utilities.split_bed_into_equal_regions.open")
+    mocked_fh.return_value.__enter__.return_value = file_opener
+    outdir = tmp_path_factory.mktemp("split_bed")
+    out_files = split(
+        infile="region.bed", outfiles=os.path.join(outdir, "x.bed"), num=3
+    )
+    assert out_files == [os.path.join(outdir, f"{i}.x.bed") for i in (1, 2, 3)]
+    assert WRITTEN_LINES == expected_outlines
